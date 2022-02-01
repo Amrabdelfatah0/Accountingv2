91b16d8a80a285436836cc17065f283b0b87d812
ahmedsalahaly <93048544+ahmedsalahaly@users.noreply.github.com>
2022-02-01 21:06:33 +0200

    deferredExpenses

---
+++ ConsoleApp1/DeferredFinance/deferredExpenses.cs
@@ -0,0 +1,16 @@
+using ConsoleApp1.bases;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.DeferredFinance
+{
+    public class deferredExpenses : Parent
+
+    {
+        public string deferredExpensesCalculation { get; set; }
+        public string expense account { get; set; }
+
+
+    }
+}
