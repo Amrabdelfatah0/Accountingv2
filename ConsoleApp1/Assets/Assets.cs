d8cd99f6f45aaaf72cb4cc96b721460f6804da7b
Mohamed Osama <73117933+mohamedosamaeg@users.noreply.github.com>
2022-02-01 23:00:14 +0500

    MohamedCommit1

---
+++ ConsoleApp1/Assets/Assets.cs
@@ -0,0 +1,20 @@
+﻿using ConsoleApp1.bases;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.Assets
+{
+    public class assets : BaseWithName
+    {
+        public int OriginalAmount { get; set; }
+        public string Currency { get; set; }
+        public string Method { get; set; }
+        public string Duration { get; set; }
+        public DateTime StartDraining { get; set; }
+        public string FixedAssetAccount { get; set; }
+        public string DrainingAccount { get; set; }
+        public string ExpenseAccount { get; set; }
+        public string Daily { get; set; }
+    }
+}
---
+++ ConsoleApp1/Banks/BankExpenses.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ConsoleApp1.bases;
+
+namespace ConsoleApp1.Banks
+{
+    internal class BankExpenses : BaseWithName
+    {
+        public string Type { get; set; }
+        public bool Checkout { get; set; }
+        public string DiaryAvailability { get; set; }
+        public string NewItem { get; set; }
+        
+    }
+}
