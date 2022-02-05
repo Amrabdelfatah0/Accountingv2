500bc654a1e6eeeac9e00b9b23060d73890e8cbb
Amr Mohamed <88450778+Amrabdelfatah0@users.noreply.github.com>
2022-02-05 21:03:50 +0200

    ed.8

--- ConsoleApp1/DeferredFinance/Acount.cs
+++ ConsoleApp1/DeferredFinance/Acount.cs
@@ -1,11 +1,14 @@
-﻿using System;
+﻿using ConsoleApp1.bases;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace ConsoleApp1.DeferredFinance
 {
-    public class Acount
+    public class Acount: BaseWithName
     {
         public List<DeferredRevenue> DeferredRevenue { get; set; }
+
+        public string AcountType { get; set; }
     }
 }
